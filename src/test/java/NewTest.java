057458a6ee8885d86fe69233bcd5efdcdd2a0272
nir eini <nirr.eini@gmail.com>
2018-12-07 11:34:59 +0200

    new project is created

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	 @Test
+	 public void test01() {
+	 	  
+//	 	  System.setProperty("webdriver.chrome.com", "C:\\Users\\Coderz-test\\Desktop\\chromedriver_win32 (4)\\chromedriver.exe");
+//	 	  WebDriver driver = new ChromeDriver();
+//	 	  driver.get("https://www.google.com/");
+//	 	  
+//	 	  long id = Thread.currentThread().getId();
+//	 	  System.out.println(" Thread number for test01 is " + id);
+//	 	  driver.quit();
+		 
+		 System.out.println("hellow world");
+	   }
+}
